23537ec4540cd6a886a68dce00a460f02ed43b80
daw2-montanez23 <113450955+daw2-montanez23@users.noreply.github.com>
2023-03-14 17:19:36 +0000

    Updates

--- src/componentes/header.js
+++
@@ -1,7 +0,0 @@
-export const header = {//exportamos una constante llamada header con un template con mi nombre y un script con un console.log
-    template: `<p>Alumno: Francesc Montañez Rosell</p>`,
-    script: ()=>{
-        console.log('Header')
-        
-    }
-}
